3376e872d2936f0d9c01bbb91721aff03b9922bd
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2023-10-12 15:49:26 +0000

    Overwriteen datatype_conversions

--- basics/datatype_conversions.js
+++ basics/datatype_conversions.js
@@ -7,21 +7,13 @@ let varDate = new Date() // Object of class Date
 const varObj= {
     "name":"Parthib",
     "Roll":27,
-    "Dept":"I.T"
+    "Dept":"I.T"                    
 }
 
 const arr = ["ABCD","XYZ",123,89] // Array
-// console.log(varObj)
-// console.log(String(varObj))
-// console.log(Number(varObj)) //  Nan because JS has forcefully converted it into a number which is not a number in reality
+console.log(varObj)
+console.log(String(varObj))
+console.log(Number(varObj)) //  Nan because JS has forcefully converted it into a number which is not a number in reality
 
-// console.table([varInt,typeof varInt])
-// console.table([String(varInt),typeof String(varInt)])
-// console.table([Boolean(varInt),typeof Boolean(varInt)])
-
-// console.log(Number(varMixed)) // Nan
-
-// console.log("1"+2+2)
-// console.log("1"+2+"2")
-// console.log(1+2+"2")
-// console.log(1+"2"+"2")
+console.table([varInt,typeof varInt])
+console.table([varInt,typeof varInt])
