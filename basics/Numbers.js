d058baea96daa99dbc36fe4236566a6d0ad2e913
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2023-10-12 21:57:29 +0530

    Math Module added

--- basics/Numbers.js
+++ basics/Numbers.js
@@ -2,6 +2,13 @@
 let numVar = 20;
 console.table([numVar,typeof numVar])
 
-numVar = new Number(20);
+numVar = new Number(2000000); // Run this example in Console to see the methods / function applied in it
 console.log(numVar);
 console.table([Number(numVar.toString()),typeof numVar])
+console.log(numVar.toFixed(2))
+console.log(numVar.toLocaleString('en-In')) // Showing a number according to a specifc country
+
+console.log(Math) // Math object -- Run this line in console to see the functions of math
+console.log((Math.random()*10)+1)
+// Here Math.random() will print random values from 0 -> 1 . Multiplied by 10, it will be from 0 -> 10
+// But if we strictly dont want any values below 1 then we are adding 1 after it
