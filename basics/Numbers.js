8b0a2200a235143a906f59cc75f3724985367e5b
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2023-10-12 15:48:12 +0000

    Added More files

---
+++ basics/Numbers.js
@@ -0,0 +1,7 @@
+// There are 2 ways of Declaring Numbers in Js
+let numVar = 20;
+console.table([numVar,typeof numVar])
+
+numVar = new Number(20);
+console.log(numVar);
+console.table([Number(numVar.toString()),typeof numVar])
