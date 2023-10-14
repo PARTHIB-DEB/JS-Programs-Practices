31c9b15b90bd343b2a69898caac494abe9fc0332
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2023-10-14 10:00:27 +0530

    array methods added

--- basics/arrays01.js
+++ basics/arrays01.js
@@ -6,7 +6,18 @@ const myArr2 = new Array(
     1,2,1,'55ab','Parthib',"^#*#)#_",[11,12,13,14,15],{"Address":"Agarpara"}
 )
 // console.table([['myArr',myArr],['myArr2',myArr2]])
-console.log(`Array Length is ${myArr.length}`)
-console.log(`ArrayString: ${myArr.join(" ")}`)
-console.log(`Does "55ab" includes in array: ${myArr.includes("55ab")}`)
-// console.log(`Does "55ab" includes in array: ${myArr.indexOf('[11,12,13,14,15]')}`) // showing -1 as index , not true
+// console.log(`Array Length is ${myArr.length}`)
+// console.log(`ArrayString: ${myArr.join(" ")}`)
+// console.log(`Does "55ab" includes in array: ${myArr.includes("55ab")}`)
+// myArr.push(100) // Pushing / Appending 100 in the array
+// console.log(myArr)
+// myArr.unshift("xyz00") //  Inserting anything in '0'th index only or Insertion from beginning
+// console.log(myArr)
+// myArr.shift()
+// console.log(myArr) // Deletion from '0'th index
+// myArr.pop() // Deleting from end index
+// console.log(myArr)
+// console.table([['Push','shift'],['Unshift','Pop']])
+// myArr.fill('static value') // Replacing all of the array values with a static value
+// console.log(myArr);
+
