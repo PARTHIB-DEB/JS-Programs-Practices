b9d83645cc9169d52551ee5de90930ff9fa26e50
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2023-11-18 19:18:00 +0000

    loops added

---
+++ Intermediate/conditionals.js
@@ -0,0 +1,23 @@
+// Conditional statements - if-else , switch-case , Ternary Op. , Null coelascing op
+// if-else
+// if(True){
+
+// }else{
+
+// }
+
+// switch-case
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// Ternary Operator
+// x=(true)?statement1:statement2
+
+// Null Coelascing Operator (Its a fallback method which prevents of coming null as a result)
+// let var1=null ?? 10
---
+++ Intermediate/loops.js
@@ -0,0 +1,59 @@
+// There are basically three types of loops : for , while , do-while
+
+// classic for loop
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// while loop
+// while (condition) {
+    
+// }
+
+//do-while loop
+// do {
+    
+// } while (condition);
+
+// although these 3 loops are enough to iterate but there are more loops, specifically more variants of 'for' loops
+// for specific types of data
+
+// for-of loop
+
+const myArr=[1,2,3,4,5]
+const myStr="Parthib"
+const myMap = new Map()
+myMap.set('IN','India') // returns an array ['IN','India']
+myMap.set('CPP','Cplusplus') // returns another array ['CPP','Cplusplus']
+console.log(myMap);
+for (const val of myArr) {
+    console.log(val);
+}
+for (const chr of myStr) {
+    console.log(chr);
+}
+for (const [key,value] of myMap) {
+    console.log(`${key}->${value}`);
+}
+
+//for-in loop(for Objects,arrays but not for 'Map')
+const myobj={
+    "name":"Parthib",
+    "skill":"Webdev"
+}
+for (const key in myobj) {
+    console.log(`${key}:${myobj[key]}`);
+}
+for (const key in myArr) { // keys for arrays are their indexes
+    console.log(`${key}:${myArr[key]}`);
+}
+
+//for-each loop (higher-order loop -> arrays,maps)
+myMap.forEach( function(value){ // call-back function -> function has no name
+    console.log(value)
+})
+
+myArr.forEach( (value)=>{
+    console.log(value);
+})
