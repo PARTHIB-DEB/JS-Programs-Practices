76d604ecc7df9af1750c27797e54ba1b542ca8b1
PARTHIB KUMAR DEB <103876861+PARTHIB-DEB@users.noreply.github.com>
2023-12-24 10:07:31 +0000

    updated

--- advanced/getter_setter.js
+++ advanced/getter_setter.js
@@ -2,48 +2,25 @@
 // We can redefine the definitions of these two functions
 // Without setter , getter can not be defined so they should be redifned at a time in a PAIR
 
-// Modern Way
-// class person{
-//     constructor(username,email){
-//         this.username=username,
-//         console.log(`Username parameter given in Constructor is : ${username}\n\n`);
-//         console.log(`Constructor function -> Now Username is : ${this.username}\n\n`)
-//         this.email=email
-//     }
+// Although there are 3 ways , most modern way is declared here
 
-//     get username(){
-//         console.log(`Getter called  : Username is ${this._username}\n\n`)
-//     }
+class person{
+    constructor(username,email){
+        this.username=username,
+        console.log(`Username parameter given in Constructor is : ${username}\n\n`);
+        console.log(`Constructor function -> Now Username is : ${this.username}\n\n`)
+        this.email=email
+    }
 
-//     set username(username){  // More priority because first it get called before constructor function
-//         this._username=username.toUpperCase()
-//         console.log(`Setter Function called , username set to ${this._username}\n\n`)
-//     }
+    get username(){
+        console.log(`Getter called  : Username is ${this._username}\n\n`)
+    }
 
-// }
+    set username(username){  // More priority because first it get called before constructor function
+        this._username=username.toUpperCase()+"1234"
+        console.log(`Setter Function called , username set to ${this._username}\n\n`)
+    }
 
-// const pepObj=new person("parthib","pkd@pkd.com")
-// pepObj.username
-
-
-// Little old way (by function)
-function person(username,email){
-    this.username=username,
-    console.log(`Username parameter given in Constructor is : ${username}\n\n`);
-    console.log(`Constructor function -> Now Username is : ${this.username}\n\n`)
-    this.email=email
-
-    Object.defineProperty(this,'username',{
-        get:function(){
-            console.log(`Getter called  : Username is ${this.username}\n\n`)
-        },
-
-        set:function(username){
-            this._username=username.toUpperCase()
-            this.usename=this._username
-            console.log(`Setter Function called , username set to ${this._username}\n\n`)
-        }
-    })
 }
 
 const pepObj=new person("parthib","pkd@pkd.com")
